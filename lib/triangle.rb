ace817c7bb06f5fc86bb300d24addb743cb5322e
Chris Kay <chriskay25@gmail.com>
2019-06-06 15:47:51 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,31 +1,31 @@
-class Triangle
+# class Triangle
   
-  def initialize(side1,side2,side3)
-    @side1 = side1
-    @side2 = side2
-    @side3 = side3
-  end 
+#   def initialize(side1,side2,side3)
+#     @side1 = side1
+#     @side2 = side2
+#     @side3 = side3
+#   end 
   
-  def kind
-    if (@side1 + @side2 <= @side3 || @side2 + @side3 <= @side1) || (@side1 <= 0) || (@side2 <= 0) || (@side3 <= 0)
-      begin
-        raise TriangleError
-      rescue TriangleError => error 
-        return error.message 
-      end 
-    elsif @side1 == @side2 && @side1 == @side3
-      :equilateral
-    elsif (@side1 != @side2 && @side1 != @side3) && (@side2 != @side3)
-      :scalene
-    else 
-      :isosceles
-    end 
-  end 
+#   def kind
+#     if (@side1 + @side2 <= @side3 || @side2 + @side3 <= @side1) || (@side1 <= 0) || (@side2 <= 0) || (@side3 <= 0)
+#       begin
+#         raise TriangleError
+#       rescue TriangleError => error 
+#         return error.message 
+#       end 
+#     elsif @side1 == @side2 && @side1 == @side3
+#       :equilateral
+#     elsif (@side1 != @side2 && @side1 != @side3) && (@side2 != @side3)
+#       :scalene
+#     else 
+#       :isosceles
+#     end 
+#   end 
   
-  class TriangleError < StandardError
-    def message
-      "The dimensions of this triangle are not valid."
-    end 
-  end 
+#   class TriangleError < StandardError
+#     def message
+#       "The dimensions of this triangle are not valid."
+#     end 
+#   end 
   
-end
+# end
