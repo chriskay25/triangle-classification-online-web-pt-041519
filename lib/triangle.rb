66245d5b21551570603c5cdcd2bf8c706241706d
Chris Kay <chriskay25@gmail.com>
2019-06-06 15:48:12 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -30,4 +30,36 @@
   
 # end
 
+class Triangle
 
+  attr_accessor :side1, :side2, :side3
+  
+  def initialize(side1,side2,side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end 
+  
+  def kind
+    if @side1 + @side2 <= @side3 || @side2 + @side3 <= @side1
+      begin
+        raise TriangleError
+      rescue TriangleError => error 
+        return error.message 
+      end 
+    elsif @side1 == @side2 && @side1 == @side3
+      :equilateral
+    elsif (@side1 != @side2 && @side1 != @side3) && (@side2 != @side3)
+      :scalene
+    else 
+      :isosceles
+	end 
+  end 
+  
+  class TriangleError < StandardError
+    def message
+      "The dimensions of this triangle are not valid."
+    end 
+  end 
+  
+end
