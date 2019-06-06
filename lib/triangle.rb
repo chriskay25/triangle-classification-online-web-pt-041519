3a3c00e68b2e0231160ad6513263eafd54ba0873
Chris Kay <chriskay25@gmail.com>
2019-06-06 15:57:09 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,38 +1,4 @@
-# class Triangle
-  
-#   def initialize(side1,side2,side3)
-#     @side1 = side1
-#     @side2 = side2
-#     @side3 = side3
-#   end 
-  
-#   def kind
-#     if (@side1 + @side2 <= @side3 || @side2 + @side3 <= @side1) || (@side1 <= 0) || (@side2 <= 0) || (@side3 <= 0)
-#       begin
-#         raise TriangleError
-#       rescue TriangleError => error 
-#         return error.message 
-#       end 
-#     elsif @side1 == @side2 && @side1 == @side3
-#       :equilateral
-#     elsif (@side1 != @side2 && @side1 != @side3) && (@side2 != @side3)
-#       :scalene
-#     else 
-#       :isosceles
-#     end 
-#   end 
-  
-#   class TriangleError < StandardError
-#     def message
-#       "The dimensions of this triangle are not valid."
-#     end 
-#   end 
-  
-# end
-
 class Triangle
-
-  attr_accessor :side1, :side2, :side3
   
   def initialize(side1,side2,side3)
     @side1 = side1
@@ -41,7 +7,7 @@ class Triangle
   end 
   
   def kind
-    if @side1 + @side2 <= @side3 || @side2 + @side3 <= @side1
+    if (@side1 + @side2 <= @side3 || @side2 + @side3 <= @side1) || (@side1 <= 0) || (@side2 <= 0) || (@side3 <= 0)
       begin
         raise TriangleError
       rescue TriangleError => error 
@@ -53,7 +19,7 @@ class Triangle
       :scalene
     else 
       :isosceles
-	end 
+    end 
   end 
   
   class TriangleError < StandardError
